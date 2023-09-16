e313d6ea19e71b124dcae41812b4d484f4407ab2
Gospel Amanze <ollygos@gmail.com>
2023-09-17 00:46:18 +0100

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Homepage />} exact />
-        <Route path='/movie/:id' element={<Moviedetails />} />
+        <Route path='/movies/:id' element={<Moviedetails />} />
       </Routes>
     </BrowserRouter>
     </>
